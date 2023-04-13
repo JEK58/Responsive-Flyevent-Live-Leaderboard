bf062e723c2da87869edadfe01a9fa40fbcc9291
Stephan Schöpe <socialcrap+github@posteo.de>
2023-04-13 21:43:52 +0200

    import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { TaskInfo } from "./components/TaskInfo";
 import { LiveRanking } from "./components/LiveRanking";
 import { NoData } from "./components/NoData";
